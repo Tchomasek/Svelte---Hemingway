4c0ed1e3b83ee38b91a26d87cabfad57195b9ad3
Tom <tchom.asek@centrum.cz>
2020-12-23 14:41:24 +0100

    'init'

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	//components
+	import Wave from './components/Wave.svelte'
+
+	let bookTitle  = 'xxx';
+	let waving = false
+</script>
+
+<style>
+	label {
+		position: absolute;
+		top: 1em;
+		left: 1em;
+		font-size: 0.7em;
+	}
+</style>
+
+<header>
+	<h1>HEMINGVAYOVÁTORO-ROTÁTOR</h1> 
+
+	<label for="waving"><input type="checkbox" id="waving" bind:checked={waving}>
+		waving
+	</label>
+</header>
+
+<main>
+	<h2>{bookTitle}</h2>
+
+	
+	<section class='controls'>
+	</section>
+</main>
+
+<footer class='water'>
+	<Wave where='back' animation='big' {waving}/>
+	<Wave where='middle' animation='tiny' {waving}/>
+	<img class="hem" src="./assets/hemingway.png" alt="ernie"/>
+	<Wave where='front' animation='small' {waving}/>
+	
+</footer>
