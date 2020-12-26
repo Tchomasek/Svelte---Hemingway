fe142ed905342d27681eef5cf2eb14fb9bffc068
Tom <tchom.asek@centrum.cz>
2020-12-26 17:41:18 +0100

    'final'

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,62 @@
 <script>
+
+	//imports
+	import { tweened } from 'svelte/motion';
+	import { shuffleArray } from "./functions";
+	import { cubicOut, circOut } from 'svelte/easing';
+
 	//components
 	import Wave from './components/Wave.svelte'
-
-	let bookTitle  = 'xxx';
+	import TheBook from './components/TheBook.svelte'
+	
+	
+	//vars
+	let books = localStorage.getStuff('unreadBooks')
+	let animationEnded = false
+	let bookHasBeenRead = false
+	let audion = new Audio('./assets/dsshotgn.wav')
 	let waving = false
+
+	// countdown
+	const progress = tweened(0, {
+		duration: 2500,
+		easing: circOut
+	})
+
+	//$progress.
+
+	// functions
+	const chooseBook = () => {
+		books =  localStorage.getStuff('unreadBooks')
+		shuffleArray(books)
+
+		console.log(books)
+
+		bookHasBeenRead = false
+		animationEnded = false
+
+		let index = $progress === 0 ? books.length - 1 : 0
+
+		progress.set(index).then(() => {
+			animationEnded = true
+			audion.play()
+		})
+		
+	}
+	const readBook = () => {
+		let readBooks = localStorage.getStuff('readBooks')
+		let unreadBooks = localStorage.getStuff('unreadBooks')
+
+		bookHasBeenRead = true
+
+		readBooks.push(chosenBook)
+		unreadBooks = unreadBooks.filter(item => item.id !== chosenBook.id)
+
+		localStorage.setStuff('readBooks', readBooks)
+		localStorage.setStuff('unreadBooks', unreadBooks)
+	}
+
+	$: chosenBook = books[Math.floor($progress)] || books[0]
 </script>
 
 <style>
@@ -13,10 +66,12 @@
 		left: 1em;
 		font-size: 0.7em;
 	}
+
 </style>
 
 <header>
 	<h1>HEMINGVAYOVÁTORO-ROTÁTOR</h1> 
+
 
 	<label for="waving"><input type="checkbox" id="waving" bind:checked={waving}>
 		waving
@@ -24,10 +79,13 @@
 </header>
 
 <main>
-	<h2>{bookTitle}</h2>
+	
+	<TheBook {chosenBook} {animationEnded} {bookHasBeenRead} />
 
 	
 	<section class='controls'>
+		<img on:click={chooseBook} src="./assets/gun.png" alt="gun">
+		<img on:click={readBook} src="./assets/anchor.png" alt="anchor">
 	</section>
 </main>
 
